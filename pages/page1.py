4e52dbaf25c42feb042e0bce330ec8dc542a8c5b
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2024-01-20 21:56:28 +0530

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -12,24 +12,21 @@ class SendRequests:
         self.report_file = None
 
     def display_form(self):
+        # Speech to text for each field
+        email_text = speech_to_text(language='en', use_container_width=True, just_once=True, key='email_stt')
+        subject_text = speech_to_text(language='en', use_container_width=True, just_once=True, key='subject_stt')
+        info_text = speech_to_text(language='en', use_container_width=True, just_once=True, key='info_stt')
+
         with st.form(key="request_form"):
-            # Speech to text buttons for each field
-            email_stt = speech_to_text(language='en', use_container_width=True, just_once=True, key='email_stt')
-            print(email_stt)
-            self.email = st.text_input("Your email address", value=email_stt if email_stt else self.email)
-
-            subject_stt = speech_to_text(language='en', use_container_width=True, just_once=True, key='subject_stt')
-            self.subject = st.text_input("Subject", value=subject_stt if subject_stt else self.subject)
-
+            # Form fields with pre-filled speech-to-text data
+            self.email = st.text_input("Your email address", value=email_text if email_text else self.email)
+            self.subject = st.text_input("Subject", value=subject_text if subject_text else self.subject)
             self.problem_type = st.selectbox(
                 "Type of Problem", 
                 options=("Report Content", "Legal Inquiries", "Report Copyright Infringement"),
                 key='problem_type'
             )
-
-            info_stt = speech_to_text(language='en', use_container_width=True, just_once=True, key='info_stt')
-            self.info = st.text_area("Description", value=info_stt if info_stt else self.info)
-
+            self.info = st.text_area("Description", value=info_text if info_text else self.info)
             self.report_file = st.file_uploader("Attachment (optional)", key='report_file')
             
             submit_button = st.form_submit_button("Submit")
